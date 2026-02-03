27d6cf6df57b2e3ccafbad011eb24ccc305d0a9f
Mestre dos Magos <ferminoapolinariojuliano@gmail.com>
2026-02-03 17:53:00 -0300

    Testes realizados com sucesso

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,34 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        const { debug } = render(<PostComment/>);
+        // eslint-disable-next-line testing-library/no-debugging-utils
+        debug()
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    it('Deve inserir dois comentários a listagem de posts', () => {
+        const { debug } = render(<PostComment />)
+        const valorInserido1 = 'Uau, ela é bonita mesma, aonde o senhor comprou?'
+        const valorInserido2 =  'Você pagou quantos nela ? te dou R$ 150.00 por ela'
+
+        fireEvent.change(screen.getByTestId('campo-comentario'),{
+            target: {
+                value: 'Uau, ela é bonita mesma, aonde o senhor comprou?'
+            }
+        })
+    fireEvent.click(screen.getByTestId('btn-comentar'))
+        fireEvent.change(screen.getByTestId('campo-comentario'),{
+            target: {
+                value: 'Você pagou quantos nela ? te dou R$ 150.00 por ela'
+        }
+    })
+    fireEvent.click(screen.getByTestId('btn-comentar'))
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    debug()
+    expect(screen.getByText(valorInserido1)).toBeInTheDocument()
+    expect(screen.getByText(valorInserido2)).toBeInTheDocument()
+    })
+})
